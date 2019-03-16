209d2c78614d6a84c636168ddee968b5d09bab31
Natalie Hawkins <nrhawkins@gmail.com>
2019-03-15 21:35:05 -0700

    dummy requirements.txt sphinx

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=PEP420PackageFinder.find("src/python"),
     package_dir={"": "src/python"},
     #data_files=[("data", ["install_windows/libsbnw_draw.dll",]),
-    package_data={"libsbml_draw": ["c_api/data/libsbml_draw.dll"]},
-    install_requires=["ctypes", "numpy", "matplotlib", "python-libsbml"],
+    package_data={"libsbml_draw.c_api": ["data/libsbml_draw.dll"]},
+    install_requires=["ctypes", "matplotlib", "numpy", "python-libsbml"],
     extras_require={
         "testing": ["pytest", "pytest-mock"],
         "documentation": ["sphinx", "sphinx_rtd_theme", "sphinx-autobuild", "sphinxcontrib-napoleon"],
