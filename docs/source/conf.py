0e767524b56bc5e51856ae8ae92dae4d2ee809cc
Natalie Hawkins <nrhawkins@gmail.com>
2019-03-15 20:48:29 -0700

    mock ctypes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,11 +16,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath("../../src/python/"))
 
-#import mock
+import mock
 
-#MOCK_MODULES = ['python-libsbml', 'ctypes']
-#for mod_name in MOCK_MODULES:
-#    sys.modules[mod_name] = mock.Mock()
+MOCK_MODULES = ['ctypes']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 #
 # -- Project information -----------------------------------------------------
