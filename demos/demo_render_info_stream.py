b97904d4c557027c55ad2282368202a0f6aaa1e5
Natalie Hawkins <nrhawkins@gmail.com>
2019-08-16 18:27:33 -0700

    adding render stream demo

--- demos/demo_render_info_stream.py
+++ demos/demo_render_info_stream.py
@@ -2,7 +2,16 @@ import libsbml
 
 info = libsbml.LocalRenderInformation()
 
-stream = libsbml.XMLInputStream("/Development/sbwBuild/source/SBMLLayout/SBMLExtension/SBGNstyles.xml")
+#stream = libsbml.XMLInputStream("/Development/sbwBuild/source/SBMLLayout/SBMLExtension/SBGNstyles.xml")
+stream = libsbml.XMLInputStream("SBGNstyles.xml")
 
 info.read(stream)
+
+print("lri: ", info.getLevel(), info.getVersion())
+
+les = info.getListOfLineEndings()
+
+print("les: ", type(les), len(les))
+
+
 
