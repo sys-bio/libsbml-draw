225d9a1d40e46c225120db107ca9888c329d2338
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2020-01-28 19:34:49 -0800

    bugfix in setup.py and add licence file. GPL3 for now.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from distutils.core import setup
 from setuptools import PEP420PackageFinder
 
-exec(open("src/python/libsbml_draw/version.py").read())
-
 MAJOR = 0
 MINOR = 0
 MICRO = 0
@@ -14,14 +12,13 @@ setup(
     version=version,
     packages=PEP420PackageFinder.find("src/python"),
     package_dir={"": "src/python"},
-    package_data={"libsbml_draw.c_api": ["libs/libsbml_draw.dll", "libs/libsbml_draw.dylib", "libs/libsbml_draw.so"],
+    package_data={"libsbml_draw.sbnw": ["libs/libsbml_draw.dll", "libs/libsbml_draw.dylib", "libs/libsbml_draw.so"],
                   "libsbml_draw.model": ["libs/*.xml"]},
     install_requires=open('requirements.txt').readlines(),
     extras_require={
         "testing": ["nose"],
         "documentation": ["sphinx", "sphinx_rtd_theme", "sphinx-autobuild", "sphinxcontrib-napoleon"],
     },
-
     zip_safe=False,
     classifiers=[
         "Intended Audience :: Science/Research",
